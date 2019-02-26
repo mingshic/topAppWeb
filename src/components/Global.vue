59f8a9cf4699cf1e2a0750684eac3ad504062782
mingshic <13667636243@163.com>
2019-02-26 10:21:32 +0800

    Update Global.vue

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -1,10 +1,6 @@
 <script>
-// const Server='http://localhost:8081';
-//const Server='http://47.104.200.60/api/proxy';
-//const Server='http://47.104.236.209:8084';
-// const Server='http://139.129.207.35:8081/topAppWebProxy'
-// const Server='http://192.168.0.100:8081'
-const Server='http://111.203.122.61:8081/topAppServer'
+
+const Server=
 
 
 const proxyServer=Server+'/api/proxy';
